a3eecefaff0d11e500a5582355609b6ae1c6db6a
stardocs-services <amos.ang@stardocs.net>
2022-02-20 15:57:02 +0800

    next commit

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
-const {readFile, writeFile} = require('fs').promises;
-const util = require('util');
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(readFile);
+const EventEmitter = require('events');
 
-// const getText = (path) => {
-//     return new Promise((resolve,reject)=>{
-//         readFile(path,'utf-8', (err,data)=>{
-//             if(err){
-//                 reject(err);
-//             }else{
-//                 resolve(data);
-//             }
-//         });
-//     });
-// }
+const customEmitter = new EventEmitter();
 
-// getText('./content/newfile1.txt')
-// .then(result=>console.log(result))
-// .catch(err=>console.log(err));
+customEmitter.on('response',()=>{
+    console.log(`Data Received`);
+});
 
-const start = async() => {
-    try{
-        const first = await readFile('./content/newfile1.txt', 'utf-8');
-        const second = await readFile('./content/newfile2.txt', 'utf-8');
-        await writeFile(
-            './content/result-newfile.txt',
-            `HELLO WORLD: ${first} ${second}`,
-            {flag: 'a'});
-        console.log(first, second);
-    }catch(error){
-        console.log(error);
-    }
-}
-
-start();
+customEmitter.emit('response');
